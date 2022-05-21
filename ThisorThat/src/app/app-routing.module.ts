f2b1921ad4916816ab1ee7ab69548301e949d8f4
jjkoh92 <68405872+jjkoh92@users.noreply.github.com>
2022-05-20 17:08:42 -0700

    Added routes

--- ThisorThat/src/app/app-routing.module.ts
+++ ThisorThat/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './category/category.component';
+import { ItemComponent } from './item/item.component';
+import { DailyQuestionComponent } from './daily-question/daily-question.component';
+import { StandingComponent } from './standing/standing.component';
 
 const routes: Routes = [
-  {path: '', component: AppComponent}
+  {path: '', component: AppComponent},
+  {path: 'category', component:CategoryComponent},
+  {path: 'item', component: ItemComponent},
+  {path: 'dailyquestion', component:DailyQuestionComponent},
+  {path: 'standing', component:StandingComponent}
 ];
 
 @NgModule({
